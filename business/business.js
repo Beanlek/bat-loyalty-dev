21025a615a53b8e7738f1f9aafdf70176a58f4f9
Ubuntu <ubuntu@ip-172-31-26-63.ap-southeast-1.compute.internal>
2024-09-10 02:16:31 +0000

    fix for merge

--- business/business.js
+++ business/business.js
@@ -3,7 +3,7 @@ const business = {};
 business.users = require('./user.js');   
 business.outlets = require('./outlet.js');
 business.user_account = require('./user_account.js');
-business.account = require('./account.js');
+business.accounts = require('./account.js');
 business.products = require('./product.js');   
 
 module.exports = business;
